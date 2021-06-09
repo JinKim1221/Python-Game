602181828b5dca40647e973a652813a2429270ea
JinKim1221 <sdo7329@gmail.com>
2021-06-09 23:23:49 +0100

    set up pygame

---
+++ pygame_basic/1_create_frame.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init() # neccessary
+
+# screen setting
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# set title
+pygame.display.set_caption("Python Game")
