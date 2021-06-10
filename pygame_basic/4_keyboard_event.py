ef97ed3a19c77465931c22829264831cb7d1de55
JinKim1221 <sdo7329@gmail.com>
2021-06-10 17:28:59 +0100

    keyboard event for the character

--- pygame_basic/4_keyboard_event.py
+++ pygame_basic/4_keyboard_event.py
@@ -18,6 +18,10 @@ character_height = character_size[1]
 character_x_pos = (screen_width / 2) - (character_width / 2) # position that is a half of the screen
 character_y_pos = screen_height - character_height # position that is at the bottom of the screen
 
+# character position to be moved
+to_x = 0
+to_y = 0
+
 # set title
 pygame.display.set_caption("Python Game")
 
@@ -27,6 +31,24 @@ while running:
     for event in pygame.event.get(): # what kind of event occured?
         if event.type == pygame.QUIT: # the event is that when closing the window
             running = False
+        if event.type == pygame.KEYDOWN: # if key is pressed
+            if event.key == pygame.K_LEFT: # move the character to the left
+                to_x -= 5
+            elif event.key == pygame.K_RIGHT: # move the character to the right
+                to_x += 5
+            elif event.key == pygame.K_UP: # move the character to up
+                to_y -= 5
+            elif event.key == pygame.K_DOWN: # move the character to down
+                to_y += 5
+        if event.type ==pygame.KEYUP: # key is unpressed
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x =0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y =0
+                
+
+    character_x_pos += to_x
+    character_y_pos += to_y
 
     screen.blit(background, (0,0)) # draw the background
     screen.blit(character, (character_x_pos, character_y_pos))
