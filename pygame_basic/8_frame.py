b02be518bb798b8352403b3db335d425eacbf0ba
JinKim1221 <sdo7329@gmail.com>
2021-06-13 23:09:55 +0100

    make a frame for games

---
+++ pygame_basic/8_frame.py
@@ -0,0 +1,47 @@
+# Frame per second
+import pygame
+##########################################################################
+################# General Initialization that you must do#################
+pygame.init() # neccessary initialization
+
+# screen setting
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# set title
+pygame.display.set_caption("Python Game")
+
+# FPS
+clock = pygame.time.Clock()
+##########################################################################
+
+##########################################################################
+# 1. user game initialization (background, game images, position, font, font, etc)
+
+
+
+
+# Event loop to prevent closing the window
+running = True # game is running
+while running:
+    delta = clock.tick(30) # set frames per second
+
+    # 2. Dealing with the events such as keyboard, mouse and so on
+    for event in pygame.event.get(): # what kind of event occured?
+        if event.type == pygame.QUIT: # the event is that when closing the window
+            running = False
+
+    # 3. Game character's position
+
+ 
+    # 4. Dealing with collision 
+    
+    
+    # 5. Draw on the window
+
+    pygame.display.update() # redraw the background
+
+
+# game finished
+pygame.quit()
